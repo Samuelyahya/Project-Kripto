9aa518f0e19270ddbfdb923e7ccf3746c2619995
Samuel Yahya <samuelyahya25@gmail.com>
2024-12-15 11:12:44 +0700

    RSA Algorithm Project

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function isPrime(num) {
     for (let i = 2; i <= Math.sqrt(num); i++) {
         if (num % i === 0) {
             return false;
-        }    
+        }
     }
     return true;
 }
